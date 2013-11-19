8a0555e1e8c8565bd2e89915201d26dcf9775c72
HashtagA <ash@ntswincash.com>
2013-11-19 10:44:40 +0100

    Initial

---
+++ PP_Notensystem/Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace PP_Notensystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("GDT");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("APHM");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("INF");
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("NWTL");
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("4aITX", new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2,
+            treeNode3,
+            treeNode4});
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("GDT");
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("APHM");
+            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("INF");
+            System.Windows.Forms.TreeNode treeNode9 = new System.Windows.Forms.TreeNode("NWTL");
+            System.Windows.Forms.TreeNode treeNode10 = new System.Windows.Forms.TreeNode("4bITT", new System.Windows.Forms.TreeNode[] {
+            treeNode6,
+            treeNode7,
+            treeNode8,
+            treeNode9});
+            System.Windows.Forms.TreeNode treeNode11 = new System.Windows.Forms.TreeNode("GDT");
+            System.Windows.Forms.TreeNode treeNode12 = new System.Windows.Forms.TreeNode("APHM");
+            System.Windows.Forms.TreeNode treeNode13 = new System.Windows.Forms.TreeNode("INF");
+            System.Windows.Forms.TreeNode treeNode14 = new System.Windows.Forms.TreeNode("NWTL");
+            System.Windows.Forms.TreeNode treeNode15 = new System.Windows.Forms.TreeNode("4cITI", new System.Windows.Forms.TreeNode[] {
+            treeNode11,
+            treeNode12,
+            treeNode13,
+            treeNode14});
+            System.Windows.Forms.TreeNode treeNode16 = new System.Windows.Forms.TreeNode("GDT");
+            System.Windows.Forms.TreeNode treeNode17 = new System.Windows.Forms.TreeNode("APHM");
+            System.Windows.Forms.TreeNode treeNode18 = new System.Windows.Forms.TreeNode("INF");
+            System.Windows.Forms.TreeNode treeNode19 = new System.Windows.Forms.TreeNode("NWTL");
+            System.Windows.Forms.TreeNode treeNode20 = new System.Windows.Forms.TreeNode("3aITX", new System.Windows.Forms.TreeNode[] {
+            treeNode16,
+            treeNode17,
+            treeNode18,
+            treeNode19});
+            System.Windows.Forms.TreeNode treeNode21 = new System.Windows.Forms.TreeNode("GDT");
+            System.Windows.Forms.TreeNode treeNode22 = new System.Windows.Forms.TreeNode("APHM");
+            System.Windows.Forms.TreeNode treeNode23 = new System.Windows.Forms.TreeNode("INF");
+            System.Windows.Forms.TreeNode treeNode24 = new System.Windows.Forms.TreeNode("NWTL");
+            System.Windows.Forms.TreeNode treeNode25 = new System.Windows.Forms.TreeNode("3bITT", new System.Windows.Forms.TreeNode[] {
+            treeNode21,
+            treeNode22,
+            treeNode23,
+            treeNode24});
+            System.Windows.Forms.TreeNode treeNode26 = new System.Windows.Forms.TreeNode("GDT");
+            System.Windows.Forms.TreeNode treeNode27 = new System.Windows.Forms.TreeNode("APHM");
+            System.Windows.Forms.TreeNode treeNode28 = new System.Windows.Forms.TreeNode("INF");
+            System.Windows.Forms.TreeNode treeNode29 = new System.Windows.Forms.TreeNode("NWTL");
+            System.Windows.Forms.TreeNode treeNode30 = new System.Windows.Forms.TreeNode("3cITI", new System.Windows.Forms.TreeNode[] {
+            treeNode26,
+            treeNode27,
+            treeNode28,
+            treeNode29});
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(262, 29);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(604, 439);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(532, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Neue Klasse";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(613, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Klasse bearb.";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(710, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Neues Fach";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 22);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Notensystem";
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 29);
+            this.treeView1.Name = "treeView1";
+            treeNode1.Name = "Knoten6";
+            treeNode1.Text = "GDT";
+            treeNode2.Name = "Knoten7";
+            treeNode2.Text = "APHM";
+            treeNode3.Name = "Knoten8";
+            treeNode3.Text = "INF";
+            treeNode4.Name = "Knoten9";
+            treeNode4.Text = "NWTL";
+            treeNode5.Name = "Knoten0";
+            treeNode5.Text = "4aITX";
+            treeNode6.Name = "Knoten11";
+            treeNode6.Text = "GDT";
+            treeNode7.Name = "Knoten12";
+            treeNode7.Text = "APHM";
+            treeNode8.Name = "Knoten13";
+            treeNode8.Text = "INF";
+            treeNode9.Name = "Knoten14";
+            treeNode9.Text = "NWTL";
+            treeNode10.Name = "Knoten1";
+            treeNode10.Text = "4bITT";
+            treeNode11.Name = "Knoten16";
+            treeNode11.Text = "GDT";
+            treeNode12.Name = "Knoten17";
+            treeNode12.Text = "APHM";
+            treeNode13.Name = "Knoten18";
+            treeNode13.Text = "INF";
+            treeNode14.Name = "Knoten19";
+            treeNode14.Text = "NWTL";
+            treeNode15.Name = "Knoten2";
+            treeNode15.Text = "4cITI";
+            treeNode16.Name = "Knoten25";
+            treeNode16.Text = "GDT";
+            treeNode17.Name = "Knoten26";
+            treeNode17.Text = "APHM";
+            treeNode18.Name = "Knoten27";
+            treeNode18.Text = "INF";
+            treeNode19.Name = "Knoten28";
+            treeNode19.Text = "NWTL";
+            treeNode20.Name = "Knoten3";
+            treeNode20.Text = "3aITX";
+            treeNode21.Name = "Knoten29";
+            treeNode21.Text = "GDT";
+            treeNode22.Name = "Knoten30";
+            treeNode22.Text = "APHM";
+            treeNode23.Name = "Knoten31";
+            treeNode23.Text = "INF";
+            treeNode24.Name = "Knoten32";
+            treeNode24.Text = "NWTL";
+            treeNode25.Name = "Knoten4";
+            treeNode25.Text = "3bITT";
+            treeNode26.Name = "Knoten33";
+            treeNode26.Text = "GDT";
+            treeNode27.Name = "Knoten34";
+            treeNode27.Text = "APHM";
+            treeNode28.Name = "Knoten35";
+            treeNode28.Text = "INF";
+            treeNode29.Name = "Knoten36";
+            treeNode29.Text = "NWTL";
+            treeNode30.Name = "Knoten5";
+            treeNode30.Text = "3cITI";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode5,
+            treeNode10,
+            treeNode15,
+            treeNode20,
+            treeNode25,
+            treeNode30});
+            this.treeView1.Size = new System.Drawing.Size(244, 439);
+            this.treeView1.TabIndex = 16;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(657, 475);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "DATE";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(829, 475);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "USER";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(751, 475);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "TIME";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(791, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 21;
+            this.button4.Text = "CSV-Import";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tableLayoutPanel1.SetColumnSpan(this.dataGridView1, 2);
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.tableLayoutPanel1.SetRowSpan(this.dataGridView1, 2);
+            this.dataGridView1.Size = new System.Drawing.Size(598, 433);
+            this.dataGridView1.TabIndex = 22;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(878, 494);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Notensystem #A+";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+
+
+    }
+}
+
