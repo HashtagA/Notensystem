7349b6254eee399ff59792cc657a22d53df0e5f5
Anton Schuster <ash@ntswincash.com>
2013-11-20 13:09:47 +0100

    Commit Structure
    Changed names form Forms
    Added Folder for Forms
    added Folder for Recources like Icons

---
+++ PP_Notensystem/Forms/Form_GegenstandGesamtAnsicht.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PP_Notensystem
+{
+    public partial class Form_GegenstandGesamtAnsicht : Form
+    {
+        public Form_GegenstandGesamtAnsicht()
+        {
+            InitializeComponent();
+        }
+
+        private void dgvGeneralView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ PP_Notensystem/Forms/Form_Hauptmaske.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using PP_Notensystem.cls;
+
+namespace PP_Notensystem
+{
+    public partial class Form_Hauptmaske : Form
+    {
+        public Form_Hauptmaske()
+        {
+            InitializeComponent();
+            DataBase.connect();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DataBase.disconnect();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            addClassForm acf = new addClassForm();
+            acf.Show();
+        }
+    }
+}
