a39ce07c245ffe3e58bdf6123a25a860ef106f2e
MS <ash@ntswincash.com>
2013-11-26 11:13:58 +0100

    DB zugriff GUI_Übersichtsform

--- PP_Notensystem/Forms/Form_GegenstandGesamtAnsicht.cs
+++ PP_Notensystem/Forms/Form_GegenstandGesamtAnsicht.cs
@@ -6,14 +6,18 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using PP_Notensystem.FormElements;
 
 namespace PP_Notensystem
 {
     public partial class Form_GegenstandGesamtAnsicht : Form
     {
+        public DataBase mainDb;
+
         public Form_GegenstandGesamtAnsicht()
         {
             InitializeComponent();
+            mainDb = new DataBase("192.168.28.130", 3306, "Hashtag", "Hashtag", "notensystem");
         }
 
         private void dgvGeneralView_CellContentClick(object sender, DataGridViewCellEventArgs e)
